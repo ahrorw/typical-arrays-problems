5552856dd33424d8046674f5a895489de0b9d984
Akhror Sharafutdinov <rgras@mail.ru>
2020-10-02 11:01:18 +0300

    init: Task Typical arrays problems finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (myArr) {
+  if ((myArr === null) || (myArr === undefined) || (!Array.isArray(myArr)) || (myArr.length === 0)) return 0;
+
+  myArr.sort(function(a, b) {
+    return a - b;
+  });
+
+  return myArr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (myArr) {
+  if ((myArr === null) || (myArr === undefined) || (!Array.isArray(myArr)) || (myArr.length === 0)) return 0;
+
+  myArr.sort(function(a, b) {
+    return a - b;
+  });
+
+  return myArr[myArr.length-1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (myArr) {
+  if ((myArr === null) || (myArr === undefined) || (!Array.isArray(myArr)) || (myArr.length === 0)) return 0;
+
+  //let arrAvg = ;
+
+  return eval(myArr.join('+')) / myArr.length;
 }
